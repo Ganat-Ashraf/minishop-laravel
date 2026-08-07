86b967d66b9916c3bdea3ee373417c4c0a31472e
Ganat Ashraf <gannat.ashraf74@gmail.com>
2026-08-07 13:54:06 +0300

    update create form fields

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <form action="/admin/store" method="POST">
     @csrf
-    <input type="text" name="name" placeholder="اسم المنتج" required>
+    <input type="text" name="name" placeholder="product name" required>
     <input type="number" step="0.01" name="price" placeholder="السعر" required>
     <input type="text" name="category" placeholder="التصنيف مثل LIFESTYLE" required>
     <input type="text" name="description" placeholder="الوصف" required>
